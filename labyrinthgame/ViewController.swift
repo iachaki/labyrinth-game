5d783b3ae19b67e6c4d0c5ae6667eb3a6550f536
Ia Kawasaki <ia_0212justin-716cherry@softbank.ne.jp>
2015-05-14 23:57:23 +0900

    Initial Commit

---
+++ labyrinthgame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  labyrinthgame
+//
+//  Created by ia on 2015/05/14.
+//  Copyright (c) 2015年 Life is tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
